7bbaddfb847652b9191103fd44da3c4f821c5c87
Nathan Morton <nathan.j.morton@gmail.com>
2020-12-28 22:58:59 -0500

    this

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
+import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+const App = () => {
+  useEffect(() => {
+    // Init Materialize JS
+    M.AutoInit();
+  });
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className='container'>
+        <AddBtn />
+        <AddLogModal />
+        <Logs />
+      </div>
+    </Fragment>
+  );
+};
+
+export default App;
