d1c7bffcf40c5997304ee6ea1fba14cec6588fb5
Nathan Morton <nathan.j.morton@gmail.com>
2020-12-30 14:51:48 -0500

    this

---
+++ src/actions/techActions.js
@@ -0,0 +1,42 @@
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+  UPDATE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  SEARCH_LOGS,
+  GET_TECHS,
+  ADD_TECH,
+  DELETE_TECH,
+  TECHS_ERROR,
+} from './types';
+
+// Get techs from server
+export const getTechs = () => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch('/techs');
+    const data = await res.json();
+
+    dispatch({
+      type: GET_TECHS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response.statusText,
+    });
+  }
+};
+
+// Set loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
---
+++ src/reducers/techReducer.js
@@ -0,0 +1,39 @@
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+  UPDATE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  SEARCH_LOGS,
+  GET_TECHS,
+  ADD_TECH,
+  DELETE_TECH,
+  TECHS_ERROR,
+} from '../actions/types';
+
+const initialState = {
+  techs: null,
+  loading: false,
+  error: null,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case GET_TECHS:
+      return {
+        ...state,
+        techs: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+};
